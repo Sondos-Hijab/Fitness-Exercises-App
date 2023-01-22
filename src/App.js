d8732c292f3ecd0e12d1f7b5471ec1e7eb76cbeb
Sondos-Hijab <sondos.hijab@gmail.com>
2023-01-22 13:21:21 +0200

    preparing the components & the containers of the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { Navbar, Brand, CTA } from "./components";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
 const App = () => {
   return (
-    <div><h1>GPT-3</h1></div>
-  )
-}
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+  );
+};
 
 export default App;
